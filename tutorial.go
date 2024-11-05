86db7881a5a885a11dab17efba4952fa8556cc0b
Twinklehingu <twinkle.hingu29@gmail.com>
2024-11-04 19:34:24 -0800

    datatypes and explicite and implicit

--- tutorial.go
+++ tutorial.go
@@ -4,5 +4,25 @@ import "fmt" // used for displaying/print
 
 //entry point
 func main() {
-	fmt.Println("Hello, world!") //print new line for printing "" and '' to show characters
+	// var name string = "Hello Twinkle!"
+	// var number uint16 = 255
+	// number = number + 34
+	// var name string
+	// name = "Hello Twinkle!" // assign value to variable
+	// name = "Bill"
+	// fmt.Println("Hello, world!") //print new line for printing "" and '' to show characters
+	// fmt.Println(name, number)
+
+	// var number = 20000.98
+	num := "hello"
+	fmt.Printf("%T", num)
 }
+
+/*
+
+The go build command compiles the Go code and produces an executable file (tutorial.exe on Windows). It does not run the program; it only creates an executable you can run later.
+This step is useful if you want to compile the program once and run it multiple times without having to recompile each time.
+
+
+
+*/
