9c2f105269fcffec93341f89c2ac4849c6c15ac0
Twinklehingu <twinkle.hingu29@gmail.com>
2024-11-04 19:19:46 -0800

    Initial commit

---
+++ tutorial.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt" // used for displaying/print
+
+//entry point
+func main() {
+	fmt.Println("Hello, world!") //print new line for printing "" and '' to show characters
+}
